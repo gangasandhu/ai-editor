dd69721742e2734f5d4e0c4600539003ebd5ee3d
Ganga Singh <sr.gangasingh@gmail.com>
2024-03-23 00:35:37 -0700

    Update vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-editor',
+  base: '/smart-script',
   plugins: [react()],
   test: {
     environment: 'jsdom',
